eb8d8f2ffb9b870d0216a462acd199ce5a418719
Isaaker <77550577+Isaaker@users.noreply.github.com>
2022-08-26 16:08:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="refresh" content="5;url=/ghost.php" />
+<title>Redirigiendote al Simulador</title>
+</head>
+
+<body>
+        <?php
+        echo "<center><h1>Por favor espere...</h1></center>";
+        echo "<hr>";
+        echo "<center><b>En breves momentos seras redirigido a la pagina principal del simulador, también puedes usar el siguiente enlace para ser redirigido de inmediato:</center></b>";
+        echo "<br>";
+        echo "<center><button> <a href='/ghost.php'>Acceder ahora</a></button></center>";
+        ?>
+</body>
+</html>

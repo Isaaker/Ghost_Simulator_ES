7a80abc4520f9c059354ddbdeeecfe263230217a
Isaac Hernán <77550577+Isaaker@users.noreply.github.com>
2023-10-01 20:23:52 +0200

    Update menu

--- home.php
+++ home.php
@@ -30,8 +30,8 @@
 
 <main>
 	<header>
+		<img src="/images/Archery_Ghost_Simulator.png" alt="ghost simulator logo" width="50" height="50"></img>
 		<div class="text">
-			<img src="/images/Archery_Ghost_Simulator.png" alt="ghost simulator logo" width="50" height="50"></img>
 			Archery Ghost Simulator
 		</div>
 		

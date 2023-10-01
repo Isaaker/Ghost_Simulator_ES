3c034f08fbdee03344261eadcdf4440579eb2a85
Isaac Hernán <77550577+Isaaker@users.noreply.github.com>
2023-10-01 20:31:59 +0200

    update menu

--- home.php
+++ home.php
@@ -34,6 +34,8 @@
 		Archery Ghost Simulator
 		<div class="UI_icons">
 			<a href="/about.html"><img src="/images/ui_icons/info.svg" alt="about"><img></a>
+			<a href="/settings.html"><img src="/images/ui_icons/settings.svg" alt="settings"><img></a>
+			<a href="https://ghostsimulator.eu.auth0.com/oidc/logout"><img src="/images/ui_icons/log-out.svg" alt="logout"><img></a>
 		</div>
 		
 	</header>

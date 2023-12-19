5ba2207ea78800a184cadf7cb99ebb26d06cf7b0
Isaaker <77550577+Isaaker@users.noreply.github.com>
2023-12-19 12:34:54 +0100

    Adapting to Flask
    Making big changes to adapt the platform to Flask. New images, new files structure, new app routes, adding CSP (Content Security Policy) and other new features and bug resolutions...

--- ghost_simulator/__init__.py
+++ ghost_simulator/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask
+from flask import *
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True,  
+            static_folder='static',
+            template_folder='templates')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -19,10 +21,41 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
+    ### APP ROUTES ###
+
+    # Index
 
     @app.route('/index')
+    @app.route('/index.html')
+    @app.route('/index.php')
+    @app.route('/')
     def index():
         return 'Hello, World!'
+        
+    # Login
 
+    @app.route('/login.html')
+    @app.route('/login')
+    def login_page():
+        return render_template("/login.html")
+
+    # Simulator Home
+
+    @app.route('/home.html')
+    @app.route('/home')
+    def home_page():
+        return render_template("/home.html")
+
+    # About
+
+    @app.route('/about.html')
+    @app.route('/about')
+    def about_page():
+        return render_template("/about.html")
+    
+    
+    
+    
+    
     return app
-
